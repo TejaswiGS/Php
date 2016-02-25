8d8ae03634ec247f9f485b97aa2959b8b1d29f58
Theju <teju77coorg@gmail.com>
2016-02-25 11:36:37 +0530

    Commit

---
+++ Starter Site1/b.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+    <style>
+        h1 {
+            padding-left: 0px;
+            font-family: Verdana;
+            text-align: left;
+            font-size: 30px;
+            color: white;
+            background: linear-gradient(to right, black, white);
+        }
+            </style>
+
+        </head>
+
+
+<?php
+
+echo "<h1>Bengaluru</h1>";
+$doc = new DOMDocument();
+$doc->load('http://weather.yahooapis.com/forecastrss?p=INXX0012&u=c');
+$channel = $doc->getElementsByTagName("channel");
+foreach($channel as $chnl)
+{
+$item = $chnl->getElementsByTagName("item");
+foreach($item as $itemgotten)
+{
+$describe = $itemgotten->getElementsByTagName("description");
+$description = $describe->item(0)->nodeValue;
+echo $description;
+}
+}
+?>
+
